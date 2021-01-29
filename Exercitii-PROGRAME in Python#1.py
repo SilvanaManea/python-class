8fa19d5963c91237e83f7ece6d77552c559de323
Silvana Manea <41144171+SilvanaManea@users.noreply.github.com>
2021-01-29 15:52:25 +0200

    Add files via upload

---
+++ Exercitii-PROGRAME in Python#1.py
@@ -0,0 +1,26 @@
+#1)Creati cate o variabila de tipul: string, int, float.
+#Variabila de tip int va retine valoarea 20
+x = int(20)
+print(x)
+#Variabila de tip float va retine valoarea 8.73
+y = float(8.73)
+print(y)
+#Variabila de tip String va retine valoarea "Python"
+st = str("Python")
+print(st)
+#Variabila de tip String va retine valoarea "Imi place programrea in"
+st1= str("Imi place programarea in " + st) 
+print(st1)
+#Variabila de tip String va retine valoarea "pentru ca suma acestor 2 numere este: "
+print("Pentru ca suma acestor 2 numere este:", x + y) 
+
+#2)Utilizand functia type determinati tipul urmatoarelor variabile
+notaA = -1
+print(type(notaA))
+
+test = 7.23
+print(type(test))
+
+curs = "Introducere in Programare"
+print(type(curs))
+
